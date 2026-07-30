50ef919a083ae2ff582378b816bd10e0048c8e33
ggddrregc-a11y <ggddrregc@github.com>
2026-07-30 12:51:40 +0000

    fix: check download result before playing media

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -122,6 +122,10 @@ async def play_hndlr(
         else:
             await sent.edit_text(m.lang["play_downloading"])
             file.file_path = await yt.download(file.id, video=video)
+            if not file.file_path:
+                return await sent.edit_text(
+                    m.lang["error_no_file"].format(config.SUPPORT_CHAT)
+                )
 
     await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
     if not tracks:
